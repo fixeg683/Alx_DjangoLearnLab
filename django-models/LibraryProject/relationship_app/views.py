585d930656f4fb1892e167e338acef5caf7077bc
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-09-05 09:43:04 +0300

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
-from .models import Book
 
-# Function-based view (list all books)
-def list_books(request):
-    # The checker expects exactly Book.objects.all()
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+# Create your views here.
