0f2d97ee5bd55f95a765612aadc7486370fe87f5
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-10-01 10:08:18 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,25 +6,26 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="post_list"),
+    # Posts CRUD
+    path("", views.PostListView.as_view(), name="post_list"),  # Homepage
     path("posts/", views.PostListView.as_view(), name="post_list"),
     path("posts/new/", views.PostCreateView.as_view(), name="post_create"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("posts/<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
     path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 
-    # Comments
+    # Comments CRUD
     path("posts/<int:post_pk>/comments/new/", views.comment_create, name="comment_create"),
-    path("comments/<int:pk>/edit/", views.comment_update, name="comment_update"),
+    path("comments/<int:pk>/update/", views.comment_update, name="comment_update"),
     path("comments/<int:pk>/delete/", views.comment_delete, name="comment_delete"),
 
-    # Auth
+    # Authentication
     path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="blog:post_list"), name="logout"),
     path("profile/", views.profile, name="profile"),
 
-    # Tags & search
+    # Tags & Search
     path("tags/<slug:tag>/", views.PostListView.as_view(), name="posts_by_tag"),
     path("search/", views.search, name="search"),
 ]
