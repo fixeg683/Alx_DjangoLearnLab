9fcb05dddb97787ab1491dcf01899ef842b7f0e1
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-10-06 07:31:56 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 # blog/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "blog"
@@ -21,4 +22,10 @@ urlpatterns = [
 
     # Tags
     path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
+
+    # 🔐 Authentication
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="blog:post_list"), name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 ]
