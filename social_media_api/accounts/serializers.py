272650426ff75d0f6e74dc49c75f255d389d0ca2
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-10-08 07:26:16 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,39 +1,55 @@
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
+    """Serializer to represent user data"""
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers_count', 'following_count']
-
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+        fields = ['id', 'username', 'email']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
-    token = serializers.SerializerMethodField()
+    """Handles user registration and token creation"""
+
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token']
+        fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        # create_user handles hashing the password correctly
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        # Generate authentication token
+        Token.objects.create(user=user)
         return user
 
-    def get_token(self, obj):
-        refresh = RefreshToken.for_user(obj)
-        return str(refresh.access_token)
+
+class LoginSerializer(serializers.Serializer):
+    """Handles user authentication and token retrieval"""
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        # Get or create auth token
+        token, created = Token.objects.get_or_create(user=user)
+        data['token'] = token.key
+        return data
